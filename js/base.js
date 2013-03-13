d49359d9e5a08d620180db31824bda4bc67beb41
Ricardo Modesto <rrmodesto@hotmail.com>
2013-03-13 19:16:36 -0300

    .

--- js/base.js
+++ js/base.js
@@ -2,4 +2,5 @@ $(function(){
 	$('.selectEstado').dropkick();
 	$('.selectCidade').dropkick();
 	$('form').customForm();
+
 });
--- js/home.js
+++ js/home.js
@@ -1,12 +1,40 @@
 $(function(){
-	var contador = 2;
-	$('.next').click(function(e){
+	var contador = 0;
+	var listaImoveisDinamica = 0;
+	
+	var listaImoveisVenda = $('#imoveisVenda ul.listaImoveis > li').length;
+	$('#imoveisVenda ul.listaImoveis').css('width',listaImoveisVenda * 222 + (listaImoveisVenda*18));
+	
+	var listaImoveisAluga = $('#imoveisAluga ul.listaImoveis > li').length;
+	$('#imoveisAluga ul.listaImoveis').css('width',listaImoveisAluga * 222 + (listaImoveisAluga*18));
+	/*alert(listaImoveisVenda);*/
+
+	$('.controle').click(function(e){
 		e.preventDefault();
 
-		contador = contador + 4;
-		$(this).parent().children('.prev').show();
+		contador = parseInt($(this).attr('rel'));
+		$(this).parent().children('.controle').show();
+		$('ul.listaImoveis').removeClass('atv');
+		$(this).parent().children('ul.listaImoveis').addClass('atv');
+		listaImoveisDinamica = $('ul.atv > li').length;
+
+
+		if($(this).hasClass('next')){
+			contador = contador + 4;
+		}
+		else{
+			contador = contador - 4;
+		}
+
+		if(contador >= listaImoveisDinamica - 4){
+			$('.next').hide();
+		}
+		if(contador == 2){
+			$('.prev').hide();
+		}
+
 		$(this).parent().children('ul.listaImoveis').animate({'margin-left':-contador * 222 - (contador *18)},500);
 		
-
+		$(this).attr('rel',contador);
 	});
 });
