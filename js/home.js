ed76daf0c47c2db571d52cb296b51cb7411484a3
Ricardo Modesto <rrmodesto@hotmail.com>
2013-03-14 20:24:22 -0300

    desenvolvimento

--- js/home.js
+++ js/home.js
@@ -1,4 +1,5 @@
 $(function(){
+
 	var contador = 0;
 	var listaImoveisDinamica = 0;
 	
@@ -9,32 +10,45 @@ $(function(){
 	$('#imoveisAluga ul.listaImoveis').css('width',listaImoveisAluga * 222 + (listaImoveisAluga*18));
 	/*alert(listaImoveisVenda);*/
 
-	$('.controle').click(function(e){
+
+	/* =================== seta a posição de cada li ========================= */
+	var v;
+	for(v=0; v<=listaImoveisVenda; v++){
+		$('ul.listaImoveis > li').eq(v).css('left',222*v + (v*18));
+	}
+	var a;
+	for(a=0; a<=listaImoveisAluga; a++){
+		$('ul.listaImoveis > li').eq(a).css('left',222*a + (a*18));
+	}
+
+	$('#conteudo').delegate(".controle","click", function(e){
 		e.preventDefault();
-
-		contador = parseInt($(this).attr('rel'));
-		$(this).parent().children('.controle').show();
-		$('ul.listaImoveis').removeClass('atv');
-		$(this).parent().children('ul.listaImoveis').addClass('atv');
-		listaImoveisDinamica = $('ul.atv > li').length;
-
+		
+		var posicaoUltimo = parseInt($('#imoveisVenda ul.listaImoveis > li:last-child').attr('style').replace('left:','').replace('px;',''));
+		var posicaoPrimeiro = parseInt($('#imoveisVenda ul.listaImoveis > li:first-child').attr('style').replace('left:','').replace('px;',''));
+		/*console.log(posicaoPrimeiro);*/
 
 		if($(this).hasClass('next')){
-			contador = contador + 4;
+			contador++;
+			$(this).parent().children('ul.listaImoveis').animate({'margin-left':-contador * 222 - (contador *18)},500, function(){
+			var clonado = $('#imoveisVenda ul.listaImoveis > li').eq(0).clone();
+			$('#imoveisVenda ul.listaImoveis > li').eq(0).remove();
+			$('#imoveisVenda ul.listaImoveis').append(clonado);
+			$('#imoveisVenda ul.listaImoveis > li:last-child').css('left',posicaoUltimo + 240);
+			});
+			
 		}
 		else{
-			contador = contador - 4;
+			contador--;
+			$(this).parent().children('ul.listaImoveis').animate({'margin-left':-contador * 222 - (contador *18)},500);
+			var clonado = $('#imoveisVenda ul.listaImoveis > li:last-child').clone();
+			$('#imoveisVenda ul.listaImoveis > li:last-child').remove();
+			$('#imoveisVenda ul.listaImoveis').prepend(clonado);
+			$('#imoveisVenda ul.listaImoveis > li:first-child').css('left',posicaoPrimeiro - 240);
 		}
 
-		if(contador >= listaImoveisDinamica - 4){
-			$('.next').hide();
-		}
-		if(contador == 2){
-			$('.prev').hide();
-		}
 
-		$(this).parent().children('ul.listaImoveis').animate({'margin-left':-contador * 222 - (contador *18)},500);
-		
-		$(this).attr('rel',contador);
+
 	});
+
 });
